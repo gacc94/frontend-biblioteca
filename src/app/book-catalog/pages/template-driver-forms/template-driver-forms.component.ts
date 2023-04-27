b958c57ea9677c9dffc39cfa356ad58564e26c80
Gustavo Caqui <gustavocaqui94@gmail.com>
2023-04-26 20:56:22 -0500

    agregando funciones a template forms

--- src/app/book-catalog/pages/template-driver-forms/template-driver-forms.component.ts
+++ src/app/book-catalog/pages/template-driver-forms/template-driver-forms.component.ts
@@ -7,6 +7,9 @@ import {NgForm} from "@angular/forms";
     styleUrls: ['./template-driver-forms.component.scss']
 })
 export class TemplateDriverFormsComponent {
+    public captureFile: Array<any> = [];
+    previewImage:any;
+
     user = {
         name: '',
         email: '',
@@ -22,6 +25,30 @@ export class TemplateDriverFormsComponent {
     validations(field: any) {
         console.log(field.control.value);
         return field;
+
+    }
+    changeFile(event: Event):void {
+        console.log(event);
+        let inputFile = (<HTMLInputElement>event.target);
+        let listFile:FileList | null = inputFile.files
+        let nameFile;
+        let sizeFile;
+        const reader = new FileReader();
+        if(listFile){
+            nameFile = listFile[0].name;
+            sizeFile = `${Math.round(listFile[0].size / 1024)} MB`;
+            reader.readAsDataURL(listFile[0]);
+        }
+        this.captureFile.push(nameFile);
+        console.log({event, nameFile});
+        console.log(this.captureFile);
+        console.log({sizeFile})
+
+        reader.onload = (ev) => {
+            const preview = reader.result as string;
+            this.previewImage = preview;
+            console.log(ev.target?.result)
+        };
 
     }
 }
